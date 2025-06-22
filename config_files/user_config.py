d8dab424c11551c86e47687966d909933f12c6e6
AThrowAwayJoke <mailstudipity@gmail.com>
2025-06-22 15:58:04 -0400

    minor documentation updates

--- config_files/user_config.py
+++ config_files/user_config.py
@@ -2,7 +2,7 @@
 This file contains user-level configuration.
 It is expected that the user fills this file once when setting up the project, and does not need to modify it after.
 Note that there is an additional config file located in mkw-scripts/mkw_config.py to mark the region of your MKW game file.
-""" # TODO: remove excess config file and port into user config
+""" # TODO: remove excess mkw_config.py file and port into user config
 
 import os
 from pathlib import Path
@@ -35,7 +35,7 @@ windows_TMLoader_profile_name = "default"
 game_path = "C:\\Games\\Dolphin-x64\\Games\\MKW_Copies\\Mario_Kart_Wii.rvz"
 
 # If on windows, path where the Dolphin exe can be found.
-# Usually Path(os.path.expanduser("~") / "AppData" / "Local" / "TMLoader" / "TMLoader.exe"
+# Usually Path(os.path.expanduser("~")) / "AppData" / "Local" / "TMLoader" / "TMLoader.exe"
 windows_dolphinexe_path = Path(os.path.expanduser("~")) / "Documents" / "Python" / "MKW_linesight" / "dolphin-stable" / "Dolphin.exe"
 
 # Name of the Dolphin executable to run
