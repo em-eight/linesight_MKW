a750b73f96a2a572a266535bb3b336971c5fb0f9
AThrowawayJoke <hirandomguy1@yahoo.com>
2025-12-14 08:40:12 -0500

    reenabled shaped reward

--- MKW_rl/experience_replay/experience_replay_interface.py
+++ MKW_rl/experience_replay/experience_replay_interface.py
@@ -43,24 +43,24 @@ class Experience:
         self,
         state_img: npt.NDArray,
         state_float: npt.NDArray,
-        # state_potential: float,
+        state_potential: float,
         action: int,
         n_steps: int,
         rewards: npt.NDArray,
         next_state_img: npt.NDArray,
         next_state_float: npt.NDArray,
-        # next_state_potential: float,
+        next_state_potential: float,
         gammas: npt.NDArray,
         terminal_actions: int,
     ):
         self.state_img = state_img
         self.state_float = state_float
-        # self.state_potential = state_potential
+        self.state_potential = state_potential
         self.action = action
         self.n_steps = n_steps
         self.rewards = rewards
         self.next_state_img = next_state_img
         self.next_state_float = next_state_float
-        # self.next_state_potential = next_state_potential
+        self.next_state_potential = next_state_potential
         self.gammas = gammas
         self.terminal_actions = terminal_actions
